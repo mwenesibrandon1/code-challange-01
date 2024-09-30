b019d6af7b3a5c0962276b1ad84cb79705066c6d
mwenesibrandon1 <brandon.mwenesi@student.moringaschool.com>
2024-09-30 11:27:10 +0300

    Add files via upload

---
+++ codechallange/grade generator.js
@@ -0,0 +1,25 @@
+let grade = 30
+
+if (grade>=0 && grade<=40){
+    console.log("E");
+}
+
+else if (grade>=40 && grade<=49){
+    console.log("D");
+}
+
+else if (grade>=49 && grade<=59){
+    console.log("C");
+}
+
+else if (grade>=60 && grade<=79){
+    console.log("B");
+}
+
+else if (grade>=80 && grade<=100){
+    console.log("A");
+}
+
+else{
+    console.log("Invalid input");
+}
